6d77d047b8a4d3fa42874c0943489733381ec664
Iskandar <iskandjuma@gmail.com>
2022-01-13 20:37:06 +0300

    adds page markup and image slicing

--- gulpfile.js
+++ gulpfile.js
@@ -15,7 +15,6 @@ var svgstore = require("gulp-svgstore")
 var posthtml = require("gulp-posthtml");
 var include = require("posthtml-include");
 var del = require("del");
-var terser = require("gulp-terser");
 
 gulp.task("css", function () {
   return gulp.src("source/sass/style.scss")
@@ -30,14 +29,6 @@ gulp.task("css", function () {
     .pipe(server.stream());
 });
 
-gulp.task("scripts", function () {
-  return gulp.src("source/js/main.js")
-    .pipe(terser())
-    .pipe(rename("main.min.js"))
-    .pipe(gulp.dest("build/js"))
-    .pipe(sync.stream());
-})
-
 gulp.task("server", function () {
   server.init({
     server: "build/",
@@ -50,7 +41,6 @@ gulp.task("server", function () {
   gulp.watch("source/sass/**/*.{scss,sass}", gulp.series("css"));
   gulp.watch("source/img/icon-*.svg", gulp.series("sprite", "html", "refresh"));
   gulp.watch("source/*.html", gulp.series("html", "refresh"));
-  gulp.watch("source/js/script.js", gulp.series("scripts"));
 });
 
 gulp.task("refresh", function (done) {
@@ -79,7 +69,7 @@ gulp.task("webp", function () {
 gulp.task("sprite", function () {
   return gulp.src("source/img/{icon-*,htmlacademy*}.svg")
     .pipe(svgstore({inlineSvg: true}))
-    .pipe(rename("sprite_auto.svg"))
+    .pipe(rename("sprite-auto.svg"))
     .pipe(gulp.dest("build/img"));
 });
 
@@ -107,5 +97,5 @@ gulp.task("clean", function () {
   return del("build");
 });
 
-gulp.task("build", gulp.series("clean", "copy", "css", "scripts", "sprite", "html"));
+gulp.task("build", gulp.series("clean", "copy", "css", "sprite", "html"));
 gulp.task("start", gulp.series("build", "server"));
--- source/js/script.js
+++ source/js/script.js
@@ -1,15 +1,25 @@
 'use strict';
-var pageHeader = document.querySelector('.page-header');
-var headerToggle = document.querySelector('.page-header__toggle');
 
-pageHeader.classList.remove('page-header--nojs');
+(function () {
 
-headerToggle.addEventListener('click', function () {
-  if (pageHeader.classList.contains('page-header--closed')) {
-    pageHeader.classList.remove('page-header--closed');
-    pageHeader.classList.add('page-header--opened');
-  } else {
-    pageHeader.classList.add('page-header--closed');
-    pageHeader.classList.remove('page-header--opened');
+  var navMain = document.querySelector('.main-nav');
+  var navToggle = document.querySelector('.main-nav__toggle');
+
+  if (navMain) {
+    navMain.classList.remove('main-nav--nojs');
   }
-});
+
+  if (navToggle) {
+    navToggle.addEventListener('click', function () {
+      if (navMain.classList.contains('main-nav--closed')) {
+        navMain.classList.remove('main-nav--closed');
+        navMain.classList.add('main-nav--opened');
+      } else {
+        navMain.classList.add('main-nav--closed');
+        navMain.classList.remove('main-nav--opened');
+      }
+    });
+  }
+
+
+})();
