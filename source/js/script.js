dab0a40348397eb448e6ea6dba5caa1e1eba1290
Iskandar <iskandjuma@gmail.com>
2022-02-03 16:42:30 +0300

    Stylization of the page of the catalog and pages of goods

--- source/js/script.js
+++ source/js/script.js
@@ -118,6 +118,8 @@ import Swiper from 'https://unpkg.com/swiper@7/swiper-bundle.esm.browser.min.js'
     },
   });
 
+  // accordion
+
   const accordions = document.querySelector('.accordion');
   const accordionButtons = document.querySelectorAll('.accordion__button');
   const accordionsNojs = document.querySelector('.accordion--nojs');
@@ -135,13 +137,16 @@ import Swiper from 'https://unpkg.com/swiper@7/swiper-bundle.esm.browser.min.js'
         const button = evt.target;
 
 
-        button.classList.add('accordion__button--active');
+        if (activeButton) {
+          activeButton.classList.remove('accordion__button--active');
+        }
+        button.classList.toggle('accordion__button--active');
 
         if (parent.classList.contains('accordion--active')) {
           parent.classList.remove('accordion--active');
           button.classList.remove('accordion__button--active');
         } else {
-          for (const j = 0; j < accordions.length; j++) {
+          for (let j = 0; j < accordions.length; j++) {
             accordions[j].classList.remove('accordion--active');
           }
           parent.classList.add('accordion--active');
@@ -149,5 +154,138 @@ import Swiper from 'https://unpkg.com/swiper@7/swiper-bundle.esm.browser.min.js'
       });
     });
   }
+
+  // filter Button
+
+  const filters = document.querySelector('.filters');
+  const filterToggle = document.querySelector('.filters__toggler');
+
+  if (filters) {
+    filters.classList.remove('filters--nojs');
+  }
+
+  if (filterToggle) {
+    filterToggle.addEventListener('click', function () {
+      if (filters.classList.contains('filters--closed')) {
+        filters.classList.remove('filters--closed');
+        filters.classList.add('filters--opened');
+      } else {
+        filters.classList.add('filters--closed');
+        filters.classList.remove('filters--opened');
+      }
+    });
+  }
+
+// Slider Goods
+
+const photos = [
+  'img/large/gold-necklace-large.jpg',
+  'img/large/pretty-gold-large.jpg',
+  'img/large/womens-necklace-large.jpg'
+];
+
+const thumbnails = document.querySelectorAll('.goods__photo-preview');
+const fullPhoto = document.querySelector('.goods__review-full');
+
+const addThumbnailClickHandler = function (thumbnail, photo) {
+  thumbnail.addEventListener('click', function () {
+    fullPhoto.src = photo;
+  });
+};
+
+for (var i = 0; i < thumbnails.length; i++) {
+  addThumbnailClickHandler(thumbnails[i], photos[i]);
+}
+
+
+// Modal
+
+  var modalLink = document.querySelector('.user-menu__button');
+  var feedbackPopup = document.querySelector('.modal-login');
+  var modalClose = document.querySelector('.modal__close');
+  var userName = document.querySelector('input[data-login-input]');
+  var modalForm = document.querySelector('.modal__form');
+  var userPassword = document.querySelector('input[data-pas-input]');
+
+  var isStorageSupport = true;
+  var storage = '';
+
+  try {
+    storage = localStorage.getItem('login');
+  } catch (err) {
+    isStorageSupport = false;
+  }
+
+  if (modalLink) {
+    modalLink.addEventListener('click', function (evt) {
+      evt.preventDefault();
+      feedbackPopup.classList.add('modal__show');
+
+      if (storage) {
+        userName.value = storage;
+        userPassword.focus();
+      } else {
+        userName.focus();
+      }
+    });
+  }
+
+  if (modalClose) {
+    modalClose.addEventListener('click', function (evt) {
+      evt.preventDefault();
+      feedbackPopup.classList.remove('modal__show');
+      feedbackPopup.classList.remove('modal__error');
+    });
+  }
+
+  if (modalForm) {
+    modalForm.addEventListener('submit', function (evt) {
+      if (!userName.value || !userPhone.value) {
+        evt.preventDefault();
+        feedbackPopup.classList.remove('modal__error');
+        feedbackPopup.offsetWidth = feedbackPopup.offsetWidth;
+        feedbackPopup.classList.add('modal__error');
+      } else {
+        if (isStorageSupport) {
+          localStorage.setItem('login', userName.value);
+        }
+      }
+    });
+  }
+
+  window.addEventListener('keydown', function (evt) {
+    if (evt.key === 'Escape') {
+      if (feedbackPopup.classList.contains('modal__show')) {
+        evt.preventDefault();
+        feedbackPopup.classList.remove('modal__show');
+        feedbackPopup.classList.remove('modal__error');
+      }
+    }
+  });
+
+  // Modal cart
+
+  const cartLink = document.querySelector(".goods-info__btn");
+  const popupCart = document.querySelector(".modal-cart");
+  const cartClose = popupCart.querySelector(".modal__close");
+
+  cartLink.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    popupCart.classList.add("modal__show");
+  });
+
+  cartClose.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    popupCart.classList.remove("modal__show");
+  });
+
+  window.addEventListener("keydown", function (evt) {
+    if (evt.keyCode === 27) {
+      if (popupCart.classList.contains("modal__show")) {
+        evt.preventDefault();
+        popupCart.classList.remove("modal__show");
+      }
+    }
+  });
 
 })();
